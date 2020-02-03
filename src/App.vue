0ba98bc3b9d9773952b4fb719d76aff62383f888
sutootototo2002 <sutootototo2002@163.com>
2020-02-03 21:53:09 +0800

    button commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div>
+     <zk-button>按钮</zk-button>
+     <zk-button type='success'>按钮组件1</zk-button>
+     <zk-button type='success'>按钮组件2</zk-button>
+     <zk-button type='info'>按钮组件3</zk-button>
+     <zk-button type='warning'>按钮组件3</zk-button>
+     <zk-button type='danger'>按钮组件3</zk-button>
+    </div>
+    <div>
+     <zk-button>按钮</zk-button>
+     <zk-button plain type='success'>按钮组件1</zk-button>
+     <zk-button plain type='success'>按钮组件2</zk-button>
+     <zk-button plain type='info'>按钮组件3</zk-button>
+     <zk-button plain type='warning'>按钮组件3</zk-button>
+     <zk-button plain type='danger'>按钮组件3</zk-button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="zk-button" :class="[`zk-button--${type}`,{'is-plain':plain}]"><slot></slot></button>
+</template>
+<script>
+export default {
+  name: 'ZkButton',
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    plain: {
+      type: Boolean,
+      default: false
+    }
+  },
+  created () {
+    console.log(this.type)
+  }
+}
+</script>
